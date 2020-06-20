da1a9647a71580f35a0e3f320a75d64f4c29e1e0
Lee Kirk Hawley <leekirkhawley@gmail.com>
2020-06-20 16:09:39 -0700

    Add project files.

---
+++ KOCR_Web/Controllers/OCRController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KOCR_Web.Models;
+using System.Diagnostics;
+using KOCR_Web.Services;
+
+namespace KOCR_Web.Controllers {
+    public class OCRController : Controller {
+
+        private readonly OCRService _ocrService;
+
+        public OCRController(OCRService ocrService) {
+            _ocrService = ocrService;
+        }
+
+        public IActionResult OCR() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DoOCR([FromForm] OCRViewModel model) {
+
+            _ocrService.DoOCR("C:\\\\Work\\OCR Files\\phototest.tif", "C:\\\\Work\\OCR Files\\phototest");
+
+            return Content("OCR Text");
+        }
+    }
+}
---
+++ KOCR_Web/Services/OCRService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KOCR_Web.Services {
+    public class OCRService {
+        public void DoOCR(string imageName, string outputBase) {
+            System.Diagnostics.Process p = new Process();
+            // Redirect the output stream of the child process.
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "c:\\\\Program Files\\Tesseract-OCR\\tesseract";
+            //p.StartInfo.Arguments= "C:\\\\Work\\OCR Files\\phototest.tif C:\\\\Work\\OCR Files\\phototest";
+            p.StartInfo.ArgumentList.Add(imageName);
+            p.StartInfo.ArgumentList.Add(outputBase);
+            p.Start();
+            // Do not wait for the child process to exit before
+            // reading to the end of its redirected stream.
+            // p.WaitForExit();
+            // Read the output stream first and then wait.
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+        }
+    }
+}
