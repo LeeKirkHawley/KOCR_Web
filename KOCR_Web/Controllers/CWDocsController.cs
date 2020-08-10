5539c63507deb6bcb10e83d315519cb00cf22ed9
Lee Kirk Hawley <leekirkhawley@gmail.com>
2020-08-10 13:41:47 -0700

    started CWDocs

---
+++ KOCR_Web/Controllers/CWDocsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KOCR_Web.Models;
+
+namespace KOCR_Web.Controllers {
+    public class CWDocsController : Controller {
+        public IActionResult Index() {
+            CWDocsIndexViewModel model = new CWDocsIndexViewModel();
+
+            return View(model);
+        }
+    }
+}
