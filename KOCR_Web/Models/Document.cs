a96ea7304139ad542a646c5c16230abdb6b54420
Lee Kirk Hawley <leekirkhawley@gmail.com>
2020-08-17 16:08:09 -0700

    added fileId to datatables view

--- KOCR_Web/Models/Document.cs
+++ KOCR_Web/Models/Document.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace KOCR_Web.Models {
     public class Document {
         [Key]
-        public int Id { get; set; }
+        [Column("Id")]
+        public int fileId { get; set; }
         public int userId { get; set; }
         public string originalDocumentName { get; set; }
         public string documentName { get; set; }
