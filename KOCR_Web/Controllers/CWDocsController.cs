27908d3f67034dccfae8bea24f4e0b91eb5fe7a8
Lee Kirk Hawley <leekirkhawley@gmail.com>
2020-08-17 16:20:00 -0700

    server-side datatables at least showing data

--- KOCR_Web/Controllers/CWDocsController.cs
+++ KOCR_Web/Controllers/CWDocsController.cs
@@ -187,11 +187,50 @@ namespace KOCR_Web.Controllers {
             return View(model);
         }
 
-        [HttpGet] 
+        [HttpPost] 
         public IActionResult LoadDocs() {
 
+            var draw = HttpContext.Request.Form["draw"].FirstOrDefault();
+
+            // Skip number of Rows count  
+            var start = Request.Form["start"].FirstOrDefault();
+
+            // Paging Length 10,20  
+            var length = Request.Form["length"].FirstOrDefault();
+
+            // Sort Column Name  
+            var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+
+            // Sort Column Direction (asc, desc)  
+            var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+
+            // Search Value from (Search box)  
+            var searchValue = Request.Form["search[value]"].FirstOrDefault();
+
+            //Paging Size (10,20,50,100)  
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+            int recordsTotal = 0;
+
             List<Document> docList = _context.Documents.ToList();
-            return Json(docList);
+//            return Json(docList);
+
+            //Sorting  
+            //if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection))) {
+            //    customerData = customerData.OrderBy(sortColumn + " " + sortColumnDirection);
+            //}
+            ////Search  
+            //if (!string.IsNullOrEmpty(searchValue)) {
+            //    customerData = customerData.Where(m => m.Name == searchValue);
+            //}
+
+            //total number of rows count   
+            recordsTotal = docList.Count();
+            //Paging   
+            var data = docList.Skip(skip).Take(pageSize).ToList();
+            //Returning Json Data  
+            var json = Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data });
+            return json;
         }
     }
 }
